0d55b20f7a6befa6c64f561a7301053bb95ab65b
Elmir <elmir.rasulov.2002@mail.ru>
2024-06-30 00:27:57 +0300

    add graceful shutdown

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,9 +1,13 @@
 package server
 
 import (
+	"context"
 	"log"
 	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/ELRAS1/shortlink/store"
 	"github.com/ilyakaznacheev/cleanenv"
@@ -27,13 +31,15 @@ func StartServer() *http.Server {
 	if err != nil {
 		log.Fatalln(err)
 	}
+	ctx, cancel := context.WithCancel(context.Background())
 	server := &server{}
 	server.srv = &http.Server{Addr: cfg.Port, Handler: server.Routes()}
 	server.ConfigureLogger(cfg.Loglevel, cfg.Configlog)
+	server.gracefulShutdown(cancel)
 	slog.SetDefault(server.logger)
 	log.Printf("app starting in port%s\n", server.srv.Addr)
 
-	err = server.ConfigureStore()
+	err = server.ConfigureStore(ctx)
 
 	if err != nil {
 		log.Fatalln(err)
@@ -43,12 +49,37 @@ func StartServer() *http.Server {
 	return server.srv
 }
 
-func (s *server) ConfigureStore() error {
-	s.store = store.New()
-
-	if err := s.store.Open(); err != nil {
-		return err
+func (s *server) ConfigureStore(ctx context.Context) error {
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	default:
+		s.store = store.New()
+		if err := s.store.Open(); err != nil {
+			return err
+		}
 	}
 
 	return nil
+}
+
+func (s *server) gracefulShutdown(cancel context.CancelFunc) {
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		for {
+			select {
+			case <-sig:
+				err := s.store.Db.Close()
+				if err != nil {
+					log.Println(err)
+				}
+				cancel()
+				log.Println("stopping server in graceful shutdown...")
+				return
+			default:
+				continue
+			}
+		}
+	}()
 }
--- store/store.go
+++ store/store.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 
-	// "strconv"
 	"sync"
 
 	"github.com/jmoiron/sqlx"
@@ -22,30 +21,28 @@ func New() *Store {
 	chc := cache{mu: &sync.RWMutex{}, ch: make(map[data]struct{})}
 	data := data{oldlink: "", newlink: ""}
 	return &Store{Db: &sqlx.DB{}, Cache: &chc, Data: &data}
+
 }
 
 func (s *Store) Open() error {
 	cfg := configDB{}
-	err := s.LoadEnv(&cfg)
+	err := s.loadEnv(&cfg)
 	if err != nil {
 		return err
 	}
 	dbUrl := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable",
 		cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.Name)
 	s.Db, err = sqlx.Connect("postgres", dbUrl)
-	//defer s.Db.Close()
-
 	if err != nil {
 		return err
 	}
-
 	if err = s.Db.Ping(); err != nil {
 		return err
 	}
 	return nil
 }
 
-func (s *Store) LoadEnv(cfg *configDB) error {
+func (s *Store) loadEnv(cfg *configDB) error {
 	if err := godotenv.Load(); err != nil {
 		return err
 	}
